37e4d81f9b06bc8a832d7c55239674dc938330ae
vsuarez <zerasul@gmail.com>
2024-07-02 12:48:39 +0200

    add fade effect to Logo

--- src/logo.c
+++ src/logo.c
@@ -7,7 +7,9 @@ void InitLogo(void){
 }
 void drawInitLogo(void){
     SYS_disableInts();
-    VDP_drawImageEx(BG_A,&logo,TILE_ATTR_FULL(PAL0,FALSE,FALSE,FALSE,TILE_USER_INDEX),0,0,TRUE,DMA);
+    PAL_setPalette(PAL0,logo.palette->data,DMA);
+    PAL_fadeIn(0,63,logo.palette->data,30,FALSE);
+    VDP_drawImageEx(BG_A,&logo,TILE_ATTR_FULL(PAL0,FALSE,FALSE,FALSE,TILE_USER_INDEX),0,0,FALSE,DMA);
     SYS_enableInts();
     Game.state=LOGO;
 }
@@ -18,5 +20,6 @@ void updateLogo(void){
 void drawLogo(void){
     if(frameLogoCounter>240){
         Game.state=INIT_MENU;
+        PAL_fadeOut(0,63,40,FALSE);
     }
 }
